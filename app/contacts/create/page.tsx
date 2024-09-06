6ffd46a85a8b665a372d14db18356460de60f185
98dev <ainnun.fajar@gmail.com>
2024-09-06 15:00:11 +0700

    first commit

---
+++ app/contacts/create/page.tsx
@@ -0,0 +1,12 @@
+import CreateForm from "@/components/create-form";
+
+const CreateContactPage = () => {
+  return (
+    <div className="max-w-md max-auto mt-5">
+      <h1 className="text-2xl text-center mb-2">Add New Contact</h1>
+      <CreateForm />
+    </div>
+  );
+};
+
+export default CreateContactPage;
